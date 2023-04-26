a6283fa2e3dcddfb582bfdc9babc0f58bc10d480
Vladislav Korenkov <vladnfs3@gmail.com>
2023-04-26 21:49:04 +1000

    Fix frontend init

---
+++ frontend/pages/login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Вход в аккаунт</h1>
+                <form class="space-y-4 md:space-y-6" @submit.prevent="submit">
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Эл. почта</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            placeholder="name@company.com"
+                            required
+                            v-model="email"
+                        />
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Пароль</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="••••••••"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                            required
+                            v-model="password"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-20 text-center"
+                    >
+                        Войти
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const email = ref('');
+    const password = ref('');
+
+    async function submit() {
+        const response = await $fetch('/login', { method: 'post' });
+        console.log(response);
+    }
+</script>
