389b61c11224bc6dbf0dba25aaf463491ad1a9bb
d4got10 <rusbeks@outlook.com>
2023-05-05 16:25:34 +1000

    Delete psycopg2 depedency, switch to JSON response

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import PlainTextResponse
+from starlette.responses import JSONResponse
 from backend.settings import STATIC_DIR, CLIENT_DOMAIN
 from backend.api.routes import routers
 from sqlalchemy.exc import IntegrityError
-from psycopg2.errors import UniqueViolation
 
 app = FastAPI()
 
 
 @app.exception_handler(IntegrityError)
 async def unique_violation_exception_handler(request: Request, exc: IntegrityError):
-    if isinstance(exc.orig, UniqueViolation):
-        return PlainTextResponse(f"Error: {exc._message()}", status_code=400)
-    raise exc
+    return JSONResponse(
+            status_code=400,
+            content=jsonable_encoder({"detail": f"{exc._message()}"})
+    )
 
 app.add_middleware(
     CORSMiddleware,
