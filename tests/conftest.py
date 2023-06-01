55b2d60700fbeb8f5fe83dfcc75935ffded36f93
Vladislav Korenkov <vladnfs3@gmail.com>
2023-06-02 03:56:38 +1000

    Fix initialize_session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import alembic.command
 import alembic.config
-from sqlalchemy_utils import drop_database, create_database, database_exists
+from sqlalchemy_utils import drop_database
 from backend.settings import PROJECT_ROOT
 from backend.database import engine, get_db
 from tests.base import create_user, test_admin, test_teacher, test_intern
@@ -14,10 +14,6 @@ db_connection = None
 def initialize_session():
     config = alembic.config.Config(str(PROJECT_ROOT / 'alembic.ini'))
 
-    if database_exists(engine.url):
-        drop_database(engine.url)
-        create_database(engine.url)
-
     db_connection = engine.connect()
     config.attributes['connection'] = db_connection
     alembic.command.upgrade(config, 'head')
@@ -28,10 +24,11 @@ def initialize_session():
         create_user(user, db=session)
     session.commit()
 
-    yield
-
-    drop_database(engine.url)
-    db_connection.close()
+    try:
+        yield
+    finally:
+        drop_database(engine.url)
+        db_connection.close()
 
 
 @pytest.fixture(scope='function', autouse=True)
