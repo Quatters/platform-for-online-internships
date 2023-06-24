211658699cf4c9c0e81a572514723d32ea671259
Vladislav Korenkov <vladnfs3@gmail.com>
2023-06-25 00:38:21 +1000

    Test commit

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen flex flex-row bg-red-600">
+
         <CommonSidebar :sidebar-items="sidebarItems[role]" />
         <div class="w-full">
             <CommonHeader />
