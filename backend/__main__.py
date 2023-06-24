0816d38330aee504133506c28c265309bd0d5849
Vladislav Korenkov <vladnfs3@gmail.com>
2023-06-24 18:48:31 +1000

    Create docker-compose.yaml

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,16 +1,5 @@
 import uvicorn
-from argparse import ArgumentParser
-from backend.settings import UVICORN_CONFIG, APP_NAME
+from backend.settings import UVICORN_CONFIG
 
 
-parser = ArgumentParser(APP_NAME)
-parser.add_argument('-D', '--dev', action='store_true')
-
-args = parser.parse_args()
-
-if args.dev:
-    uvicorn.run(**UVICORN_CONFIG)
-else:
-    raise NotImplementedError(
-        'Production config is not implemented yet. Use -D/--dev to up development server.'
-    )
+uvicorn.run(**UVICORN_CONFIG)
